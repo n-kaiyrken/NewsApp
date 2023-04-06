af30a2ba4a3e4913bf2c21eb0608271ae16acf08
n-kaiyrken <111049118+n-kaiyrken@users.noreply.github.com>
2023-04-06 20:16:58 +0600

    added app module, di, api queries

---
+++ app/src/main/java/com/example/newsapp/utils/Constants.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constants {
+    companion object{
+        const val BASE_URL = "https://newsapi.org/"
+        const val API_KEY = "16f6db33d0154dd79f2266839a55ee61"
+    }
+}
